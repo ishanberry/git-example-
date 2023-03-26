8004b05c0ac0a935e0cc78d875b48ba5d3b0e92d
ishan berry <ishanberry87@gmail.com>
2023-03-26 09:21:58 +0530

    first commit

---
+++ Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void main(String[] args) {
+        int a =10;
+        int b=5;
+        int Sum = a+b;
+        System.out.print(Sum);
+        System.out.print("we are learning git and github ");
+    }
+} 
---
+++ Sum1.java
@@ -0,0 +1,10 @@
+public class Sum1 {
+    public static void main(String[] args) {
+        int a =10;
+        int b=10; //  change is here 
+        int Sum = a+b;
+        System.out.println(Sum);
+        System.out.println("this is the change in file 1 ");
+
+    }
+}
